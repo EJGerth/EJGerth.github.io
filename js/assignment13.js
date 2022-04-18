f577473a65f3494fd2e17b07ee5f995c5bf0b7c7
EJGerth <97704574+EJGerth@users.noreply.github.com>
2022-04-17 20:56:12 -0600

    JavaScript is rough
    I am not sure why it is not working, but I am sure that I didn't do anything correctly.

--- js/assignment13.js
+++ js/assignment13.js
@@ -31,6 +31,8 @@ function result() {
   if(document.getElementById("uk").checked) {
     const weight = Math.round(300/=14);
     const temperature =  Math.round(94 - 32 * 5/9);
+    replace.weight = const weight;
+    replace.temperature = const temperature;
     }
 
   story.textContent = newStory;
